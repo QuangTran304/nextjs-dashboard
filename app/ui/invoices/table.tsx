abf60b1315eb73f0960a1888d3421fefd78fdbb2
Quang Tran <nhatquang304@gmail.com>
2023-12-02 22:53:26 -0500

    updated alt for <Image />

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt="unknown alt - edit in the future"
                       />
                       <p>{invoice.name}</p>
                     </div>
